64b2aeadeeb8e62e58f6f994862531ef45c72bf3
kosta-design <66972581+kosta-design@users.noreply.github.com>
2020-08-26 13:27:41 +0200

    Add files via upload

---
+++ kosta-loading-theForest/__resource.lua
@@ -0,0 +1,17 @@
+files {
+    'loading.html',
+    'css/background.css',
+    'css/logo.css',
+    'css/menu.css',
+    'css/musicbutton.css',
+    'css/cursor.css',
+    'js/main.js',
+    'js/plugins.js',
+    'webfonts/ConcertOne-Regular.ttf',
+    'webfonts/IndieFlower-Regular.ttf',
+    'audio.mp3',
+}
+
+loadscreen 'loading.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
